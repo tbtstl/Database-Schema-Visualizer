3a9d09b6ad78c2c5f19d8d14cf98dbcc9d4ed731
Tyson Battistella <tysonbattistella@gmail.com>
2017-01-22 17:35:14 -0800

    added API endpoint for connecting to the database

--- server/app.py
+++ server/app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/connect", methods=["POST"])
+def connect():
+  data = request.get_json()
+  host = data.get('host', 'localhost')
+  db_name = data.get('dbName')
+  username = data.get('username', 'root')
+  password = data.get('password', 'password')
+
+  if not all((host, db_name, username, password)):
+    response = {'error': 'Host, database name, username, and password are all required.'}
+    return jsonify(**response)
+
+  response = {'data': data}
+  return jsonify(**response)
 
 if __name__ == "__main__":
     app.run(port=5001)
