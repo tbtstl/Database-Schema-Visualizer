aa93b1ec76ceff2a5b472a4f2c5095c3d4987473
Tyson Battistella <tysonbattistella@gmail.com>
2017-01-23 15:38:34 -0800

    HI-4 #comment added form to connect content component

--- scripts/Connect.jsx
+++ scripts/Connect.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      host: '',
+      dbName: '',
+      username: '',
+      password: ''
+    };
+  }
+
   render() {
     return (
-      <div></div>
+      <div>
+        <h3>Connect to a MySQL Database</h3>
+        <form>
+          <label className="pt-label">
+            Host
+            <input type="text" className="pt-input"/>
+          </label>
+          <label className="pt-label">
+            Database Name
+            <input type="text" className="pt-input"/>
+          </label>
+          <label className="pt-label">
+            Username
+            <input type="text" className="pt-input"/>
+          </label>
+          <label className="pt-label">
+            Password
+            <input type="password" className="pt-input"/>
+          </label>
+          <button className="pt-button pt-intent-primary pt-fill" type="submit">Connect</button>
+        </form>
+      </div>
     );
   }
 }
