9a51a4e88b9eda1ced13c2b7868c04b821405317
Tyson Battistella <tysonbattistella@gmail.com>
2017-01-22 16:51:17 -0800

    HI-1 #comment added navbar and content container

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
+import Content from './Content.jsx';
+
 export default class App extends Component {
   render() {
     return (
       // Add your component markup and other subcomponent references here.
-      <h1>Hello, World!</h1>
+      <div>
+      <nav className="pt-navbar .modifier .pt-fixed-top">
+        <div className="pt-navbar-group pt-align-left">
+          <div className="pt-navbar-heading">Database Schema Visualizer</div>
+        </div>
+        <div className="pt-navbar-group pt-align-right">
+          <button className="pt-button pt-minimal pt-intent-primary pt-icon-log-in">Connect</button>
+          <span className="pt-navbar-divider"></span>
+          <button className="pt-button pt-minimal pt-icon-cog"></button>
+        </div>
+        </nav>
+        <div>
+          <Content></Content>
+        </div>
+      </div>
     );
   }
 }
