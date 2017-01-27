9bf7cd8ab1327629141e8711e4f354c4e7a952eb
Tyson Battistella <tysonbattistella@gmail.com>
2017-01-26 19:28:47 -0800

    HI-7 #comment split server into two separate endpoints, Visualizer requests the schema once loaded

--- server/application.py
+++ server/application.py
@@ -31,27 +31,11 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-@app.route("/connect", methods=["POST", "OPTIONS"])
+@app.route("/schema", methods=["GET"])
 @crossdomain(origin='http://localhost:5000')
-def connect():
+def schema():
   schema = {}
-  data = json.loads(request.data.decode("utf-8"))
-  host = data.get('host', 'localhost')
-  db_name = data.get('dbName')
-  username = data.get('username', 'root')
-  password = data.get('password', 'password')
-
-  if not all((host, db_name, username, password)):
-    return error_response('Host, database name, username, and password are all required.')
-
-  app.config['MYSQL_USER'] = str(username)
-  app.config['MYSQL_PASSWORD'] = str(password)
-  app.config['MYSQL_DB'] = str(db_name)
-  app.config['MYSQL_HOST'] = str(host)
 
   try:
     conn = get_db()
@@ -68,5 +52,32 @@ def connect():
 
   return jsonify(**schema)
 
+
+@app.route("/connect", methods=["POST", "OPTIONS"])
+@crossdomain(origin='http://localhost:5000')
+def connect():
+  data = json.loads(request.data.decode("utf-8"))
+  host = data.get('host', 'localhost')
+  db_name = data.get('dbName')
+  username = data.get('username', 'root')
+  password = data.get('password', 'password')
+
+  if not all((host, db_name, username, password)):
+    return error_response('Host, database name, username, and password are all required.')
+
+  app.config['MYSQL_USER'] = str(username)
+  app.config['MYSQL_PASSWORD'] = str(password)
+  app.config['MYSQL_DB'] = str(db_name)
+  app.config['MYSQL_HOST'] = str(host)
+
+  try:
+    conn = get_db()
+
+  except Error as e:
+    return error_response(repr(e))
+
+  return jsonify({'success': True})
+
 if __name__ == "__main__":
     app.run(port=5001)
+
