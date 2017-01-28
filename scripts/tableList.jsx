3d6066c407e20894caf0354698835784470d7b48
Tyson Battistella <tysonbattistella@gmail.com>
2017-01-28 11:18:36 -0800

    HI-10 #comment added table list item with checkboxes

--- scripts/tableList.jsx
+++ scripts/tableList.jsx
@@ -11,6 +11,12 @@ export default class TableList extends Component {
       schema: props.schema,
       tables: props.tables
     };
+    this.onToggleVisibility.bind(this);
+  }
+
+  onToggleVisibility(tableName, isVisible){
+    console.log(tableName);
+    console.log(isVisible);
   }
 
   render() {
@@ -18,8 +24,9 @@ export default class TableList extends Component {
     console.log(tables);
     return (
       <div className="tableList pt-card pt-elevation-3">
+        <div className="pt-card pt-elevation-0"><h5 className="center-horizontal">Table List</h5></div>
         {
-          tables.map((table, index) => <TableListItem table={table} key={index}/>)
+          tables.map((table, index) => <TableListItem table={table} key={table.name} onToggleVisibility={this.onToggleVisibility}/>)
         }
       </div>
     );
