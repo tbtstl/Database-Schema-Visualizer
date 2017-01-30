21cb90a2b4b64e65a60598efc49d76d75dfe6944
Tyson Battistella <tysonbattistella@gmail.com>
2017-01-30 10:59:01 -0800

    HI-15 #comment projects are stored in cookies. If projects exist in cookies, a project select box is rendered

--- scripts/Connect.jsx
+++ scripts/Connect.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {hashHistory} from 'react-router';
+import cookie from 'react-cookie';
+
 import 'whatwg-fetch';
 
 export default class App extends Component {
@@ -7,16 +9,29 @@ export default class App extends Component {
     super(props);
 
     this.state = {
+      projectName: '',
       host: '',
       dbName: '',
       username: '',
       password: '',
       calloutClassName: '',
-      calloutText: ''
+      calloutText: '',
+      projects: {},
+      hideProjects: true, //hide
+      selectedProject: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getProjectOptions = this.getProjectOptions.bind(this);
+    this.projectSelected = this.projectSelected.bind(this);
+  }
+
+  componentWillMount(){
+    this.state.projects = cookie.load('projects') || {};
+    if(this.state.projects){
+      this.state.hideProjects = false;
+    }
   }
 
   handleChange(event) {
@@ -27,13 +42,20 @@ export default class App extends Component {
   }
 
   handleSubmit(event) {
+    event.preventDefault();
     let form = {
       host: this.state.host,
       dbName: this.state.dbName,
       username: this.state.username,
       password: this.state.password
     };
-    event.preventDefault();
+
+    if(!this.state.projects.hasOwnProperty(this.state.projectName) && this.state.projectName.length > 0){
+      let projects = this.state.projects;
+      Object.assign(projects, {[this.state.projectName]: form});
+      cookie.save('projects', projects, {path: '/'});
+    }
+
     this.setState({calloutClassName: ''});
     fetch('http://localhost:5001/connect', {
       method: 'POST',
@@ -55,7 +77,6 @@ export default class App extends Component {
         if (resp.error) {
           this.setState({calloutText: 'Could not connect: ' + resp.error});
         } else {
-          console.log('all good');
           this.setState({calloutText: 'Successfully Connected to ' + this.state.host});
           hashHistory.push('/visualizer');
         }
@@ -67,7 +88,25 @@ export default class App extends Component {
       });
   }
 
-  render() {
+  getProjectOptions(){
+    let options = [<option key="-1" value=''></option>];
+    Object.keys(this.state.projects).forEach((key, index)=>{
+      options.push(<option key={index} value={key}>{key}</option>);
+    });
+    return options;
+  }
+
+  projectSelected(e){
+    console.log(e.target.value);
+    console.log(this.state.projects);
+    let project = this.state.projects[e.target.value];
+    Object.keys(project).forEach((key)=>{
+      this.setState({[key]: project[key]})
+    })
+  }
+
+
+  render(){
     return (
       <div>
         <nav className="pt-navbar .modifier .pt-fixed-top">
@@ -75,9 +114,7 @@ export default class App extends Component {
             <div className="pt-navbar-heading">Database Schema Visualizer</div>
           </div>
           <div className="pt-navbar-group pt-align-right">
-            <button className="pt-button pt-minimal pt-intent-primary pt-icon-log-in" onClick={() => {
-              this.renderConnectForm()
-            }}>Connect
+            <button className="pt-button pt-minimal pt-intent-primary pt-icon-log-in" onClick={() => {this.renderConnectForm()}}>Connect
             </button>
             <span className="pt-navbar-divider"></span>
             <button className="pt-button pt-minimal pt-icon-cog"></button>
@@ -85,9 +122,23 @@ export default class App extends Component {
         </nav>
         <div className="pt-card pt-elevation-2 content-container">
           <h3>Connect to a MySQL Database</h3>
+          <div className={this.state.hideProjects ? "hide" : ""}>
+            <h5>Select from a previous project</h5>
+            <div className="pt-select">
+              <select onChange={this.projectSelected} value={this.state.selectedProject}>
+                {this.getProjectOptions()}
+              </select>
+            </div>
+          </div>
+          <hr />
           <form onSubmit={this.handleSubmit}>
+            <h5>Create a new project</h5>
             <div className={this.state.calloutClassName}>{this.state.calloutText}</div>
             <br/>
+            <label className="pt-label">
+              Project Name
+              <input name="projectName" value={this.state.projectName} type="text" className="pt-input" onChange={this.handleChange}/>
+            </label>
             <label className="pt-label">
               Host
               <input name="host" value={this.state.host} type="text" className="pt-input" onChange={this.handleChange}/>
