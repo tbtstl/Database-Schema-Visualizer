f5445e45d81748dc41d0f15490877c7bca27d84c
James Barlow <jambar@uvic.ca>
2017-03-13 01:47:31 -0700

    Significant fixes to the pseudocode algorithm. Small fix to OrderAscPk. /abstraction route added.

--- server/application.py
+++ server/application.py
@@ -7,6 +7,7 @@ from flask import g
 
 from db import MySQL
 from helpers import crossdomain, error_response, get_columns_for_table, get_links_from_table, OrderAscPk, pk
+from functools import reduce
 
 app = Flask(__name__)
 
@@ -118,9 +119,11 @@ def abstraction():
   Returns a JSON representation of the abstracted database schema (and it's FK links) to be used by the client application.
     If any errors occur while querying the database, a 400 response is returned with an explanation of the error.
   """
+  abstractEntities = []
+  abstractRelationships = []
+  response = {'abstractEntities': abstractEntities, 'abstractRelationships': abstractRelationships}
+
   schema = {}
-  links = {}
-  response = {'schema': schema, 'links': links}
 
   try:
     conn = get_db()
@@ -132,11 +135,6 @@ def abstraction():
       table = table[0]
       schema[table] = get_columns_for_table(conn, table)
 
-      link = get_links_from_table(conn, table)
-
-      if link:
-        links[table] = get_links_from_table(conn, table)
-
     """
     nes is the index of the last abstract entity
     nes + 1 is the index of the first abstract relationship
@@ -144,92 +142,144 @@ def abstraction():
     cluster should come out of this section with index 0 up to and including nes with entities
     """
 
-
-    disjoint = false
-    remaining_rels = schema[:]
+    disjoint = False
+    remaining_rels = schema.copy()
     ordered_rels = OrderAscPk(schema)
     cluster = []
-    cluster.append(ordered_rels.items(0))
-    del remaining_rels[ordered_rels.items(0)[0]]
+    ordered_rels_list = list(ordered_rels.items())
+    cluster.append([list(ordered_rels_list[0])])
+    del remaining_rels[ordered_rels_list[0][0]]
     nes = 0
-    for i in range(1, len(ordered_rels)):
-      R = ordered_rels.items(i)
-      if pk(R[1]) == pk(ordered_rels.items(i-1)[1]): #Might need to hash this comparison
-        cluster[nes] = R
+    
+    for i in range(1, len(ordered_rels_list)):
+      R = ordered_rels_list[i]
+      for index in range(nes+1):
+        if pk(R[1]) == pk(ordered_rels_list[index][1]):
+          break
+      if pk(R[1]) == pk(ordered_rels_list[index][1]):
+        cluster[index].append(R)
         del remaining_rels[R[0]]
       else:
-        disjoint = true
-        for S in cluster: #union of all elements in cluster up to and including nes
-          if not set(pk(R[1])).isdisjoint(pk(S[1])):
-            disjoint = false
+        disjoint = True
+        checkSummedPkR = []
+        for element in pk(R[1]):
+          checkSummedPkR.append(reduce(lambda x,y: x^y, [hash(item) for item in element.items()]))
+        checkSummedPkS = []
+        unionPkS = []
+        for j in range(nes+1):
+          for S in cluster[j]:
+            for element in pk(S[1]):
+              unionPkS.append(element)
+        for element in unionPkS:
+          checkSummedPkS.append(reduce(lambda x,y: x^y, [hash(item) for item in element.items()]))
+        if not set(checkSummedPkR).isdisjoint(checkSummedPkS):
+          disjoint = False
         if disjoint:
-          cluster.append(R)
+          cluster.append([R])
           del remaining_rels[R[0]]
           nes = nes+1
-
 
     """
     next section...
     """
 
-    for R in remaining_rels:
+    for R in list(remaining_rels.items()):
       i = 0
-      clustered = false
+      clustered = False
+      checkSummedPkR = []
+      for element in pk(R[1]):
+        checkSummedPkR.append(reduce(lambda x,y: x^y, [hash(item) for item in element.items()]))
       while (i <= nes) and not clustered:
-        if not set(pk(R[1])).isdisjoint(pk(cluster[i])):
-
+        checkSummedPkS = []
+        for table in range(len(cluster[i])):
+          for element in pk(cluster[i][table][1]):
+            checkSummedPkS.append(reduce(lambda x,y: x^y, [hash(item) for item in element.items()]))
+        if not set(checkSummedPkR).isdisjoint(checkSummedPkS):
+          checkSummedOthers = []
           for j in range(nes+1):
-              if i!=j and set(pk(R[1])).isdisjoint(pk(cluster[j])):
-                  cluster[i] = R
-                  del remaining_rels[R[0]]
-                  clustered = true
-
+            if j == i:
+              continue
+            for table in range(len(cluster[j])):
+              for element in pk(cluster[j][table][1]):
+                checkSummedOthers.append(reduce(lambda x,y: x^y, [hash(item) for item in element.items()]))
+          if set(checkSummedPkR).isdisjoint(checkSummedOthers):
+              cluster[i].append(R)
+              del remaining_rels[R[0]]
+              clustered = True
+        i = i + 1
 
     """
     last section...
     nes + 1 to nas should be relationships
     """
 
-    argument = []
-    intersects = []
-    for i in range(nes+1):
-      intersects.append(false)
-    nas = nes + 1
-    first_relationship = true
-    for R in remaining_rels:
-      for i in range(0, nes+1):
-        if not set(pk(R[1])).isdisjoint(pk(cluster[i])):
-          intersects[i] = true
-      if first_relationship:
+    if len(remaining_rels) == 0:
+      nas = nes
+    else:
+      nas = nes + 1
+      argument = []
+      for i in range(nas-nes+1):
+        innerArgument = []
+        for j in range(nes+1):
+          innerArgument.append(False)
+        argument.append(innerArgument)
+      intersects = []
+      for i in range(nes+1):
+        intersects.append(False)
+      first_relationship = True
+      for R in list(remaining_rels.items()):
+        frozDictsR = []
+        for dictionary in pk(R[1]):
+          frozDictsR.append(hash(frozenset(dictionary)))
         for i in range(0, nes+1):
-          argument[0,i] = intersects[i]
-        cluster.append(R)
-        del remaining_rels[R[0]]
-        first_relationship = false
-      else:
-        j = 0
-        found = false
-        while (j <= nas) and not found:
-          totBool = true
-          for i in range(nes+1):
-            totBool &= (intersects[i] == argument[j,i])
-          if totBool:
-            cluster.insert(nes+j, R)         
-            del remaining_rels[R[0]]
-            found = true
-          j = j + 1
-        if not found:
-          nas = nas + 1
-          for i in range(0, nes + 1):
-            argument[nas - nes,i] = intersects[i]
+          frozDictsClustI = []
+          for j in range(len(cluster[i])):
+            for dictionary in pk(cluster[i][j][1]):
+              frozDictsClustI.append(hash(frozenset(dictionary)))
+          if not set(frozDictsR).isdisjoint(frozDictsClustI):
+            intersects[i] = True
+        if first_relationship:
+          for i in range(0, nes+1):
+            argument[0][i] = intersects[i]
           cluster.append(R)
           del remaining_rels[R[0]]
+          first_relationship = False
+        else:
+          j = 0
+          found = False
+          while (j <= nas) and not found:
+            totBool = True
+            for i in range(nes+1):
+              totBool &= (intersects[i] == argument[j][i])
+            if totBool:
+              cluster[j].append(R)         
+              del remaining_rels[R[0]]
+              found = True
+            j = j + 1
+          if not found:
+            nas = nas + 1
+            for i in range(0, nes + 1):
+              argument[nas - nes][i] = intersects[i]
+            cluster[nas].append(R)
+            del remaining_rels[R[0]]
 
 
     """
     Should be abstracted from here
     """
 
+    for i in range(nes+1):
+      entity = {}
+      for table in cluster[i]:
+        entity[table[0]] = table[1]
+      abstractEntities.append(entity)
+
+    if nes != nas:
+      for i in range(nes+1,nas+1):
+        relationship = {}
+        for table in cluster[i]:
+          relationship[table[0]] = table[1]
+        abstractRelationships.append(relationship)
 
   except Error as e:
     return error_response(repr(e))
