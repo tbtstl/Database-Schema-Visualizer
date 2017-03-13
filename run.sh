e18985fa4bb8491fb8f4cb2e3a96a2b4a42915b4
Tyson Battistella <tysonbattistella@gmail.com>
2017-03-13 13:49:29 -0700

    added script to run and install application

---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+SERVER_ENV_NAME=server_environment
+
+echo 'Installing Requirements';
+apt-get update;
+
+echo 'Checking if Python 3 Installed...';
+if [ $(dpkg-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed") -eq 0 ]
+then
+  echo 'Installing Python3';
+  apt-get install python3;
+fi
+
+
+echo 'Checking if npm Installed...';
+if [ $(dpkg-query -W -f='${Status}' npm 2>/dev/null | grep -c "ok installed") -eq 0 ]
+then
+  echo 'Installing npm';
+  apt-get install npm
+fi
+
+echo "Creating python 3 virtual environment and installing requirements"
+python3 -m venv $SERVER_ENV_NAME
+source $SERVER_ENV_NAME/bin/activate
+pip3 install -r requirements.txt
+
+echo "Python requirements installed. Running server.."
+chmod +x server/application.py
+python3 server/application.py &
+
+echo "Server started";
+echo "Installing npm packages";
+npm install
+
+echo "Starting Client App...";
+npm start &
+
+python3 -mwebbrowser http://localhost:5000
+
+echo "If a web browser was not opened, navigate to http://localhost:5000 to begin using the application";
